13622380447b17dcdb4074f565c0fa5fb8054f20
Herman Tulleken <herman.tulleken@gmail.com>
2022-05-22 11:18:00 -0400

    IsSort Implemented

---
+++ DataStructures/MergeSort.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Gamelogic.Extensions;
+using JetBrains.Annotations;
+
+namespace DataStructures;
+
+public static class MergeSort
+{
+	/// <summary>
+	/// Checks whether a list is sorted.
+	/// </summary>
+	/// <param name="list">A list of elements to check. Cannot be null.</param>
+	/// <param name="comparer">A comparer to use to compare elements. If not supplied
+	/// or null, <see cref="Comparer{T}.Default"/> is used.</param>
+	/// <typeparam name="T"></typeparam>
+	/// <returns><see langword="true"/> the list is sorted, <see langword="false"/> otherwise.</returns>
+	public static bool IsSorted<T>([NotNull] IList<T> list, [CanBeNull] IComparer<T> comparer = null)
+	{
+		list.ThrowIfNull(nameof(list));
+		
+		comparer ??= Comparer<T>.Default;
+		
+		if (list.Count <= 1)
+		{
+			return true;
+		}
+		
+		//We have at least two elements
+		Debug.Assert(list.Count >= 2);
+		
+		for (int i = 1; i < list.Count; i++)
+		{
+			//Negative indexes are impossible
+			Debug.Assert(i - 1 >= 0);
+			
+			var item0 = list[i - 1];
+			var item1 = list[i];
+			
+			if(comparer.Compare(item0, item1) > 0)
+			{
+				return false;
+			}
+			
+			//All items up to i are sorted
+		}
+		
+		//All items up to the last index are sorted
+		return true;
+	}
+}
---
+++ Tests/TestMergeSort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructures;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture, TestOf(typeof(MergeSort))]
+public class TestMergeSort
+{
+	private readonly record struct SortableItemTracker(int Key, int OriginalOrder);
+
+	private class KeyComparer : IComparer<SortableItemTracker>
+	{
+		public int Compare(SortableItemTracker x, SortableItemTracker y) => x.Key - y.Key;
+	}
+
+	private class KeyOrderComparer : IComparer<SortableItemTracker>
+	{
+		public int Compare(SortableItemTracker x, SortableItemTracker y) =>
+			x.Key == y.Key
+				? x.OriginalOrder - y.OriginalOrder
+				: x.Key - y.Key;
+	}
+
+	private static List<int>[] sortedLists = 
+	{
+		new(),
+		new() {7},
+		new() {7, 7, 7, 7},
+		new() {0, 1, 2, 3, 4},
+		new() {0, 0, 1, 2, 3},
+		new() {0, 11, 23, 45, 67, 89},
+		new() {0, 11, 23, 23, 67, 67, 67, 99}
+	};
+
+	private static List<int>[] unsortedLists =
+	{
+		new(){1, 0},
+		new(){0, 1, 2, 3, 4, 5, 6, 7, 6}
+	};
+	
+	[Test]
+	[TestCaseSource(nameof(sortedLists))]
+	public static void TestIsSortedOnSortedList(IList<int> list) => TestIsSorted(list, true);
+
+	[Test]
+	[TestCaseSource(nameof(unsortedLists))]
+	public static void TestIsSortedOnUnsortedList(IList<int> list) => TestIsSorted(list, false);
+
+	[Test]
+	public static void TestNullThrows()
+	{
+		Assert.Throws<ArgumentNullException>(() => MergeSort.IsSorted<int>(null!));
+	}
+	
+	private static void TestIsSorted(IList<int> list, bool isSorted) => Assert.That(MergeSort.IsSorted(list), Is.EqualTo(isSorted));
+}
